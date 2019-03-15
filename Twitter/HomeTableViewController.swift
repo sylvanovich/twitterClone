988f73a16ee19727d58b6f61ece2c648b86d34e9
Joseph Sylvanovich <jsylvanovich@gmail.com>
2019-03-14 21:08:24 -0400

    tweet, retweet and favorite

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -21,8 +21,18 @@ class HomeTableViewController: UITableViewController {
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+        
 
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
+    
+    
     
     @objc func loadTweets(){
         
@@ -86,6 +96,8 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
 
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
         return cell
     }
 
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -13,11 +13,31 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var tweetsContent: UILabel!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var favButton: UIButton!
+    @IBOutlet weak var retweet: UIButton!
     
-    
+    @IBAction func favRetweet(_ sender: Any) {
+    }
+    @IBAction func retweetTweet(_ sender: Any) {
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    
+    
+    var favorited:Bool = false
+    var tweetId:Int = -1
+
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if (favorited) {
+            favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }
+        else {
+            favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
